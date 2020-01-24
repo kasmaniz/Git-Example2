d519d3f3bf27a4d691d03ee186ecca66e897fc8f
kasmaniz <kasmaniz@sheridancollege.ca>
2020-01-24 15:29:21 -0500

    by Zohaib Kasmani

---
+++ src/gitexample/JavaApplication11.java
@@ -0,0 +1,22 @@
+/*
+ * Zohaib Kasmani
+ * 991545984
+ * SYST10199-Web Programming
+ */
+package gitexample;
+
+/**
+ *
+ * @author Taby zeri
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("pull operation");
+    }
+    
+}
